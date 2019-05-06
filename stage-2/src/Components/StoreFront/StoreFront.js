7a231f62c9427af4ca6b601834ce189f559a2ccf
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-05-06 16:19:22 -0600

    complete

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component  } from 'react';
 import './StoreFront.css';
 
-class StoreFront extends Component {
+class StoreFront extends Component  {
+
   render() {
-    let productDisplay = this.props.products.map((element, index) => {
+    
+    
+    let productDisplay = this.props.products.map((element, index)=>{
+      
+      
       return (
+        
         <div className="product-container" key={index}>
           <h2>{element.title}</h2>
           <img src={element.image} alt="" />
@@ -16,6 +22,7 @@ class StoreFront extends Component {
     })
     return (
       <div className="storefront-container">
+      {productDisplay}
       </div>
     )
   }
